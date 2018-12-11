21109c7206d08712e488c5ef3c7699a2fcc7a845
mmrshams <mmrshams00@gmail.com>
2018-12-11 20:32:40 +0330

    tanija

--- aloMechanic/API GATEWAY/server/routes/index.js
+++ aloMechanic/API GATEWAY/server/routes/index.js
@@ -16,6 +16,20 @@ var app = express()
  
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+    
+  
+  app.get('/', function(req, res, next) {
+    // Handle the get for this route
+  });
+  
+  app.post('/', function(req, res, next) {
+   // Handle the post for this route
+  });
 
 
 const router = express.Router();
