36a917042a5ec7cb30695ac475cd2445922a38e2
mmrshams <mmrshams00@gmail.com>
2018-12-11 20:56:08 +0330

    taninja

--- aloMechanic/API GATEWAY/config/express.js
+++ aloMechanic/API GATEWAY/config/express.js
@@ -41,6 +41,13 @@ morgan.token('id', function getId (req) {
 
 // creating express instance
 const app = express();
+var cors = require('cors')
+app.use(cors);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(assignId)
 app.use(express.static(__dirname));
--- aloMechanic/API GATEWAY/server/routes/index.js
+++ aloMechanic/API GATEWAY/server/routes/index.js
@@ -9,20 +9,10 @@ import promotion  from './promotion';
 import transportation from './transportation';
 import productandservice  from './productandservice';
 import order  from './order';
-
 var express = require('express')
-var cors = require('cors')
+
 var app = express()
  
-app.use(cors())
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-    
-  
   app.get('/', function(req, res, next) {
     // Handle the get for this route
   });
@@ -30,7 +20,7 @@ app.use(function(req, res, next) {
   app.post('/', function(req, res, next) {
    // Handle the post for this route
   });
-
+  
 
 const router = express.Router();
 
