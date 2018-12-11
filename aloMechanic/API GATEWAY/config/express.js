69f3ee0e7a4e2b0b9736761b3326fdabf7c4a556
mmrshams <mmrshams00@gmail.com>
2018-12-11 21:51:07 +0330

    tainja

--- aloMechanic/API GATEWAY/config/express.js
+++ aloMechanic/API GATEWAY/config/express.js
@@ -47,6 +47,13 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  app.get('/', function(req, res, next) {
+    // Handle the get for this route
+  });
+  
+  app.post('/', function(req, res, next) {
+   // Handle the post for this route
+  });
 
 app.use(assignId)
 app.use(express.static(__dirname));
